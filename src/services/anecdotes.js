e718c8e2c587b25003c0ab4bfb3a756df2a43185
サカリ <veikka.junttila1@gmail.com>
2024-12-03 09:03:31 +0900

    [fix] jsonserver logic

--- src/services/anecdotes.js
+++ src/services/anecdotes.js
@@ -1,64 +1,24 @@
-// import axios from 'axios'
+import axios from 'axios'
 
-// const baseUrl = 'https://fs-osa11-anekdootit-pipeline.fly.dev/anecdotes'
-
-const fakeDatabase = [
-  {
-    'content': 'If it hurts, do it more often',
-    'id': '47145',
-    'votes': 20
-  },
-  {
-    'content': 'Adding manpower to a late software project makes it later!',
-    'id': '21149',
-    'votes': 49
-  },
-  {
-    'content': 'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-    'id': '69581',
-    'votes': 24
-  },
-  {
-    'content': 'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-    'id': '36975',
-    'votes': 27
-  },
-  {
-    'content': 'Premature optimization is the root of all evil.',
-    'id': '25170',
-    'votes': 112
-  },
-  {
-    'content': 'Mitä hitaammin tippuu, sen parempaa tulee',
-    'id': '31932',
-    'votes': 22
-  },
-  {
-    'content': 'Ei kahta ilman kolmatta',
-    'id': '6117',
-    'votes': 1
-  }
-]
-
+const baseUrl = 'http://localhost:3001/anecdotes'
 
 const getAll = async () => {
-  // const response = await axios.get(baseUrl)
-  // return await response.data
-  return fakeDatabase
+  const response = await axios.get(baseUrl)
+  return await response.data
 }
 
 const createNew = async (object) => {
-  // const pushedObject = {
-  //   content: object.content,
-  //   id: object.id,
-  //   votes: object.votes }
-  // const response = await axios.post(baseUrl, pushedObject)
-  // return await response.data
-  fakeDatabase.concat(object)
+  const pushedObject = {
+    content: object.content,
+    id: object.id,
+    votes: object.votes }
+  const response = await axios.post(baseUrl, pushedObject)
+  return await response.data
 }
 
 const update = async (newObject) => {
-  fakeDatabase.map(anecdote => anecdote.id === newObject.id ? newObject : anecdote)
+  const response = await axios.put(`${baseUrl}/${newObject.id}`, newObject)
+  return await response.data
 }
 
 export default { getAll, createNew, update }
